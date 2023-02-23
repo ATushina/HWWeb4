b0b4390917b29a4deaaa6f9f7c49076992076792
ATushina <115179514+ATushina@users.noreply.github.com>
2023-02-23 19:40:12 +0300

    Add files via upload

---
+++ hw.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  ( 
+  id INTEGER PRIMARY KEY, 
+  id_name TEXT NOT NULL, 
+  age INTEGER,
+  adress TEXT NOT NULL
+); 
+
+INSERT INTO groupmates VALUES (1, "Clark", 25, Moskowskaya 25); 
+INSERT INTO groupmates VALUES (2, "Dave", 37, Minskaya 25); 
+INSERT INTO groupmates VALUES (3, "Ava", 48, Kievskaya 1); 
